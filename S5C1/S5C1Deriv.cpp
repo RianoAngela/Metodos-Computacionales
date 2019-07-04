244b6516f208d007fcb2d5377083a08bada49765
RianoAngela <52290014+RianoAngela@users.noreply.github.com>
2019-07-04 08:53:40 -0500

    Create S5C1Deriv.cpp

---
+++ S5C1/S5C1Deriv.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int derivada(int)
+
+int main()
+{
+    cout << "Por favor ingrese el numero inicio del intervalo: ";
+    int numeroInicial;
+    cin >> numeroInicial;
+    cout << "Por favor ingrese el numero final del intervalo: ";
+    int numeroFinal;
+    cin >> numeroFinal;
+    
+    int array1[numeroInicial][numeroFinal]
+    int array2[]=cos(numeroFinal-numeroInicial)
+    derivada(*array1)
+    return 0
+}
+
+int derivada(int array1)
+{
+    
+    
+    return *derivada
+}
