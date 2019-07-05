696ec0518fb8374f675900995a910d85d2f02aa3
RianoAngela <52290014+RianoAngela@users.noreply.github.com>
2019-07-05 09:16:26 -0500

    Update RK4orden.cpp

--- S5C2/RK4orden.cpp
+++ S5C2/RK4orden.cpp
@@ -17,15 +17,15 @@ int main()
     float aux=0.5*h;
     for (int i=0; i<n;i++)
     {
-        float k1= h*funcion(x[i-1],y[i-1]);
-        float k2= h*funcion(x[i-1]+aux,y[i-1]+0.5*k1);
-        float k3= h*funcion(x[i-1]+aux,y[i-1]+0.5*k2);
-        float k4= h*funcion(x[i-1]+h,y[i-1]*k3);
+        float k1= h*funcion(x[i],y[i]);
+        float k2= h*funcion(x[i]+aux,y[i]+0.5*k1);
+        float k3= h*funcion(x[i]+aux,y[i]+0.5*k2);
+        float k4= h*funcion(x[i]+h,y[i]*k3);
             
         float promedio= 1/6*(k1+(2*k2)+(2*k3)+k4);
         
-        x[i] = x[i-1] + h;
-        y[i] = y[i-1] + promedio;
+        x[i] = x[i] + h;
+        y[i] = y[i] + promedio;
     }
     
     cout << x;
