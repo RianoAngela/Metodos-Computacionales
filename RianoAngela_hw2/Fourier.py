cd45c43acc17a8ae89436e83d521f30e547696d8
RianoAngela <aj.riano@uniandes.edu.co>
2019-07-17 02:49:09 +0000

    taller 2

--- RianoAngela_hw2/Fourier.py
+++ RianoAngela_hw2/Fourier.py
@@ -10,24 +10,42 @@ print(np.shape(caraseria))
 print(np.shape(carafeliz))
 
 
-#Transformada de fourier
-tfcaraseria=np.fft.fft2(caraseria).real
-tfcarafeliz=np.fft.fft2(carafeliz).real
+plt.imshow(caraseria)
+plt.savefig("cara2Original.png")
 
-plt.imshow(tfcaraseria)
-plt.imshow(tfcarafeliz)
-plt.savefig("transformadafourier.png")
+plt.imshow(carafeliz)
+plt.savefig("cara3Original.png")
 
-#Frecuencias
-frecuenciaCS=np.fft.fftfreq()
-frecuenciaCF=np.fft.fftfreq()
+##Para obtener la cara nueva 2 y 3 se baso en el proceso realizado en computo ciencias uniandes
+#proceso para cara3
+t = np.linspace(-6, 6, 10)
+foto = np.exp(-0.00025*t**2)
+foto = foto / np.trapz(foto)
 
+aumento = foto[:, np.newaxis] * foto[np.newaxis, :]
+aumento_ft = np.fft.fft2(aumento, s=carafeliz.shape[:2], axes=(0, 1))
+cara3_ft = np.fft.fft2(carafeliz)
+cara3_ft = kernel_ft2[:,:] * cara3_ft
+cara3 = np.fft.ifft2(img3_ft).real
 
-#Transformada inversa de fourier 
-cara2 = np.fft.ifft2(frecuenciaCS).real
-cara3 = np.fft.ifft2(frecuenciaCF).real
-
-fig = plt.figure(2, figsize=(7,7))
-plt.imshow(cara2)
 plt.imshow(cara3)
+plt.savefig("nuevacara3.png")
+
+#proceso para cara2
+foto1 = np.exp(-15*t**2)
+foto1 = foto1 / np.trapz(foto1)
+
+aumento1 = foto1[:, np.newaxis] * foto1[np.newaxis, :]
+aumento1_ft = np.fft.fft2(aumento1, s=caraseria.shape[:2], axes=(0, 1))
+cara2_ft = np.fft.fft2(caraseria)
+cara2_ft = kernel_ft1[:,:] * cara2_ft
+cara2 = np.fft.ifft2(img2_ft).real
+
+plt.imshow(cara2)
+plt.savefig("nuevacara2.png")
+
+#foto hibrida 
+hibrida=cara2+cara3
+
+plt.imshow(hibrida)
 plt.savefig("hibrida.png")
