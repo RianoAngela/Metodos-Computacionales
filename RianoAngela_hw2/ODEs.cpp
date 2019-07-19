71c94e7d2308cc59f536bf664f4c3732861b6400
RianoAngela <aj.riano@uniandes.edu.co>
2019-07-19 03:38:34 +0000

    taller 2

--- RianoAngela_hw2/ODEs.cpp
+++ RianoAngela_hw2/ODEs.cpp
@@ -18,7 +18,6 @@ int main ()
     rk(0.005, "rk_0.005.dat");
     rk(0.001, "rk_0.001.dat");
     rk(0.0001, "rk_0.0001.dat");
-    
     return 0;
 }
 
@@ -27,7 +26,7 @@ void euler(double dt, string nombre)
 {
     double G = 6.674*pow(10,-29);
     double M = 1.989*pow(10,30);
-    int pasos=20;
+    int pasos=100;
     double x_presente[pasos];
     double y_presente[pasos];
     double vx_presente[pasos];
@@ -47,10 +46,11 @@ void euler(double dt, string nombre)
     {
         outfile<< x_presente[i-1]<<" "<<y_presente[i-1]<<" "<<vx_presente[i-1]<<" "<<vy_presente[i-1]<<endl;
         double R = sqrt(pow(x_presente[i-1],2)+pow(y_presente[i-1],2));
+        double auxiliar= (-G*M)/pow(R,3);
         x_presente[i] = (vx_presente[i-1]*dt)+x_presente[i-1];
         y_presente[i] = (vy_presente[i-1]*dt)+y_presente[i-1];   
-        vx_presente[i] = ((((-G*M)/pow(R,3))*x_presente[i-1])*dt)+vx_presente[i-1];
-        vy_presente[i] = ((((-G*M)/pow(R,3))*y_presente[i-1])*dt)+vy_presente[i-1];
+        vx_presente[i] = ((auxiliar*x_presente[i-1])*dt)+vx_presente[i-1];
+        vy_presente[i] = ((auxiliar*y_presente[i-1])*dt)+vy_presente[i-1];
         t=t+dt;
         
     }
@@ -61,7 +61,7 @@ void leap_frog(double dt,string nombre)
 {
     double G = 6.674*pow(10,-29);
     double M = 1.989*pow(10,30);
-    int pasos=22;
+    int pasos=100;
     double x_presente[pasos];
     double y_presente[pasos];
     double vx_presente[pasos];
@@ -74,24 +74,27 @@ void leap_frog(double dt,string nombre)
     vy_presente[0] = 0.606;
     
     //paso 1
+    
     double R1 = sqrt(pow(x_presente[0],2)+pow(y_presente[0],2));
+    double auxiliar1= (-G*M)/pow(R1,3);
     x_presente[1] = (vx_presente[0]*dt)+x_presente[0];
     y_presente[1] = (vy_presente[0]*dt)+y_presente[0];   
-    vx_presente[1] = ((((-G*M)/pow(R1,3))*x_presente[0])*dt)+vx_presente[0];
-    vy_presente[1] = ((((-G*M)/pow(R1,3))*y_presente[0])*dt)+vx_presente[0];
+    vx_presente[1] = ((auxiliar1*x_presente[0])*dt)+vx_presente[0];
+    vy_presente[1] = ((auxiliar1*y_presente[0])*dt)+vy_presente[0];
     
     ofstream outfile;
     outfile.open(nombre);
     
     double t=0.0;
-    for (int i=2;i<=22;i++)
+    for (int i=2;i<=20;i++)
     {
         outfile<< x_presente[i-2]<<" "<<y_presente[i-2]<<" "<<vx_presente[i-2]<<" "<<vy_presente[i-2]<<endl;
         double R = sqrt(pow(x_presente[i-1],2)+pow(y_presente[i-1],2));
-        x_presente[i] = (vx_presente[i-1]*(2*dt))+x_presente[i-2];
-        y_presente[i] = (vy_presente[i-1]*(2*dt))+y_presente[i-2];
-        vx_presente[i] = ((((-G*M)/pow(R,3))*x_presente[i-1])*(2*dt))+vx_presente[i-2];
-        vy_presente[i] = ((((-G*M)/pow(R,3))*y_presente[i-1])*(2*dt))+vy_presente[i-2];
+        double auxiliar= (-G*M)/pow(R,3);
+        x_presente[i] = (vx_presente[i-1]*(2*dt))+x_presente[i-1];
+        y_presente[i] = (vy_presente[i-1]*(2*dt))+y_presente[i-1];
+        vx_presente[i] = ((auxiliar*x_presente[i-1])*(2*dt))+vx_presente[i-1];
+        vy_presente[i] = ((auxiliar*y_presente[i-1])*(2*dt))+vy_presente[i-1];
         t=t+dt;
         
     }
@@ -100,12 +103,80 @@ void leap_frog(double dt,string nombre)
 
 void rk(float dt, string nombre)
 {
+    double G = 6.674*pow(10,-29);
+    double M = 1.989*pow(10,30);
+    
+    double x_presente, y_presente, vx_presente, vy_presente;
+    double x_futuro, y_futuro, vx_futuro, vy_futuro;
+    double k0_x, k0_y, k0_vx, k0_vy;
+    double k1_x, k1_y, k1_vx, k1_vy;
+    double k2_x, k2_y, k2_vx, k2_vy;
+    double k3_x, k3_y, k3_vx, k3_vy;
+    double t_presente, t_futuro;
+    
+    //condiciones iniciales
+    x_presente = 0.1163;
+    y_presente = 0.9772;
+    vx_presente = -6.35;
+    vy_presente = 0.606;
+    
     ofstream outfile;
     outfile.open(nombre);
-    
-    outfile.close()
+    double t=0.0;
+    for (int i=1; i<=20;i++)
+    {
+        x_futuro = vx_presente*dt + x_presente;
+        y_futuro = vy_presente*dt + y_presente;
+        vx_futuro = (((-G*M)/pow(R,3))*x_presente)*dt+vx_presente;
+        vy_futuro = (((-G*M)/pow(R,3))*y_presente)*dt+vy_presente;
+        
+        outfile << x_futuro << " " << y_futuro << " " << vx_futuro << " " << vy_futuro << endl;
+        
+        k0_x = ;
+        k0_y = ;
+        k0_vx = ;
+        k0_vy = ;
+        
+        t_futuro = t_presente + dt*0.5;
+        x_futuro = x_presente + 0.5 * dt * k0_x;
+        y_futuro = y_presente + 0.5 * dt * k0_y;
+        vx_futuro = vx_presente + 0.5 * dt * k0_vx;
+        vy_futuro = vy_presente + 0.5 * dt * k0_vy;
+        k1_x = ;
+        k1_y = ;
+        k1_vx = ;
+        k1_vy = ;
+        
+        t_futuro = t_presente + dt*0.5;
+        x_futuro = x_presente + 0.5 * dt * k1_x;
+        y_futuro = y_presente + 0.5 * dt * k1_y;
+        vx_futuro = vx_presente + 0.5 * dt * k1_vx;
+        vy_futuro = vy_presente + 0.5 * dt * k1_vy;
+        k2_x = ;
+        k2_y = ;
+        k2_vx = ;
+        k2_vy = ;
+        
+        t_futuro = t_presente + dt;
+        x_futuro = x_presente + dt * k2_x;
+        y_futuro = y_presente + dt * k2_y;
+        vx_futuro = vx_presente + dt * k1_vx;
+        vy_futuro = vy_presente + dt * k1_vy;
+        k3_x = ;
+        k3_y = ;
+        k3_vx = ;
+        k3_vy = ;
+        
+        k_x  = k0_x/6.0 + k1_x/3.0 + k2_x/3.0 + k3_x/6.0;
+        k_y  = k0_y/6.0 + k1_y/3.0 + k2_y/3.0 + k3_y/6.0;
+        k_vx = k0_vx/6.0 + k1_vx/3.0 + k2_vx/3.0 + k3_vx/6.0;
+        k_vy = k0_vy/6.0 + k1_vy/3.0 + k2_vy/3.0 + k3_vy/6.0;
+        
+        x_futuro = x_presente + dt * k_x;
+        y_futuro = y_presente + dt * k_y;
+        vx_futuro = vx_presente + dt * k_vx;
+        vy_futuro = vy_presente + dt * k_vy;
+        t = t + dt;
+    }
+    outfile.close();
 }
-
-
-
-
--- RianoAngela_hw2/plots.py
+++ RianoAngela_hw2/plots.py
@@ -1,42 +1,74 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
-#Graficas para dt=0.005
+#importacion de los datos 
 euler1=np.genfromtxt("euler_0.005.dat")
 leap1=np.genfromtxt("leap_0.005.dat")
 rk1=np.genfromtxt("rk_0.005.dat")
 
-plt.plot(euler1[:,0],euler1[:,1])
-plt.plot(leap1[:,0],leap1[:,1])
-plt.plot(rk1[:,0],rk1[:,1])
+euler2=np.genfromtxt("euler_0.001.dat")
+leap2=np.genfromtxt("leap_0.001.dat")
+rk2=np.genfromtxt("rk_0.001.dat")
 
-plt.plot(euler1[:,2],euler1[:,3])
-plt.plot(leap1[:,2],leap1[:,3])
-plt.plot(rk1[:,2],rk1[:,3])
+euler3=np.genfromtxt("euler_0.001.dat")
+leap3=np.genfromtxt("leap_0.001.dat")
+rk3=np.genfromtxt("rk_0.001.dat")
+
+#Graficas de x vs y
+fig, (ax1, ax2, ax3, ax4, ax5, ax6, ax7, ax8, ax9) = plt.subplots(9,3)
+fig.suptitle("Graficas x vs y", fontsize=20)
+
+ax1.set_title("dt=0.005 euler")
+ax1.plot(euler1[:,0],euler1[:,1])
+ax2.set_title("dt=0.005 leap frog")
+ax2.plot(euler1[:,0],euler1[:,1])
+ax3.set_title("dt=0.005 runge kutta")
+ax3.plot(rk1[:,0],rk1[:,1])
+ax4.set_title("dt=0.001 euler")
+ax4.plot(euler2[:,0],euler2[:,1])
+ax5.set_title("dt=0.001 leap frog")
+ax5.plot(leap2[:,0],leap2[:,1])
+ax6.set_title("dt=0.001 runge kutta")
+ax6.plot(rk2[:,0],rk2[:,1])
+ax7.set_title("dt=0.0001 euler")
+ax7.plot(euler3[:,0],euler3[:,1])
+ax8.set_title("dt=0.0001 leap frog")
+ax8.plot(leap3[:,0],leap3[:,1])
+ax9.set_title("dt=0.0001 runge kutta")
+ax9.plot(rk3[:,0],rk3[:,1])
+
+plt.tight_layout()
+plt.subplots_adjust(top=0.6)
+plt.savefig("XY_met_dt.pdf")
+
+#Graficas de vx vs vy 
+fig1, (a1, a2, a3, a4, a5, a6, a7, a8, a9) = plt.subplots(9,3)
+fig1.suptitle("Velocidad en x vs velocidad en y", fontsize=20)
+
+a1.set_title("dt=0.005 euler")
+a1.plot(euler1[:,2],euler1[:,3])
+a2.set_title("dt=0.005 leap frog")
+a2.plot(euler1[:,2],euler1[:,3])
+a3.set_title("dt=0.005 runge kutta")
+a3.plot(rk1[:,2],rk1[:,3])
+a4.set_title("dt=0.001 euler")
+a4.plot(euler2[:,2],euler2[:,3])
+a5.set_title("dt=0.001 leap frog")
+a5.plot(leap2[:,2],leap2[:,3])
+a6.set_title("dt=0.001 runge kutta")
+a6.plot(rk2[:,2],rk2[:,3])
+a7.set_title("dt=0.0001 euler")
+a7.plot(euler3[:,2],euler3[:,3])
+a8.set_title("dt=0.0001 leap frog")
+a8.plot(leap3[:,2],leap3[:,3])
+a9.set_title("dt=0.0001 runge kutta")
+a9.plot(rk3[:,2],rk3[:,3])
+
+plt.tight_layout()
+plt.subplots_adjust(top=0.6)
+plt.savefig("VxVy_met_dt.pdf")
+
+#Graficas del momento angular 
 
 
-#graficas para dt=0.001
-euler1=np.genfromtxt("euler_0.001.dat")
-leap1=np.genfromtxt("leap_0.001.dat")
-rk1=np.genfromtxt("rk_0.001.dat")
-
-plt.plot(euler2[:,0],euler2[:,1])
-plt.plot(leap2[:,0],leap2[:,1])
-plt.plot(rk[:,0],rk2[:,1])
-
-plt.plot(euler2[:,2],euler2[:,3])
-plt.plot(leap2[:,2],leap2[:,3])
-plt.plot(rk2[:,2],rk2[:,3])
-
-#graficas para dt=0.0001
-euler3=np.genfromtxt("euler_0.005.dat")
-leap3=np.genfromtxt("leap_0.005.dat")
-rk3=np.genfromtxt("rk_0.005.dat")
-
-plt.plot(euler3[:,0],euler3[:,1])
-plt.plot(leap3[:,0],leap3[:,1])
-plt.plot(rk3[:,0],rk3[:,1])
-
-plt.plot(euler3[:,2],euler3[:,3])
-plt.plot(leap3[:,2],leap3[:,3])
-plt.plot(rk3[:,2],rk3[:,3])
+#Graficas de la energia total del sistema
